feac8b7656f43bad517489d0f620daab50de26c3
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-02 16:08:05 +0530

    feat: integrate Cloudinary for profile picture uploads and add update profile functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
-dotenv.config();
 
 const app = express();
 
@@ -16,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
