2a75bee41b27c7135d0578d2dadaa8b887149f8c
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-02 19:33:00 +0530

    feat: add validation for sending messages and enhance message schema

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -45,6 +45,25 @@ export const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!test && !image) {
+      return res.status(400).json({
+        message: "Text or image is required.",
+      });
+    }
+
+    if (senderId.euqals(receiverId)) {
+      return res.status(400).json({
+        message: "You cannot send message to yourself.",
+      });
+    }
+
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({
+        message: "Receiver not found.",
+      });
+    }
+
     let imageUrl;
 
     if (imageUrl) {
