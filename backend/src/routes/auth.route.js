244eee4eac5b4d600a13ebd94caec16bc4a01d67
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-02 15:11:37 +0530

    feat: implement login and logout functionality in auth controller and routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,13 @@
 import { Router } from "express";
 
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 
-router.get("/login", (req, res) => {
-  res.send("login endpoint");
-});
+router.post("/login", login);
 
-router.get("/logout", (req, res) => {
-  res.send("logout endpoint");
-});
+router.post("/logout", logout);
 
 export default router;
