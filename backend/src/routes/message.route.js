edd0d14c38299ac5a31068ec08035e16efd12c6b
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-02 19:26:15 +0530

    feat: implement message handling with CRUD operations and integrate Cloudinary for image uploads

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import {
+  getAllChatPartners,
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = Router();
 
-router.get("/send", (req,res)=>{
-  res.send("message send success")
-})
+// the middlewares execute in order - so requests get rate-limited first, then authnticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting
+router.use(arcjetProtection, protectRoute);
 
-export default router
+router.get("/contacts", getAllContacts);
+router.get("/chats", getAllChatPartners);
+router.get("/:id", getMessagesByUserId);
+
+router.post("/send/:id", sendMessage);
+
+export default router;
