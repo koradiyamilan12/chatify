35ad7ed167e11618ef6c2ac85db2c70d55f33cad
koradiyamilan12 <milankoradiya.work@gmail.com>
2026-01-02 11:59:34 +0530

    fix: update build script to specify frontend build directory refactor: change express route method for serving index.html in production

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.use((_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
